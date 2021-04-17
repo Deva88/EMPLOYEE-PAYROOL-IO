a680208fee7d0e393ccb55e03d5dc1377a17658b
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-17 17:23:19 +0530

    Welcome to EMPLOYEE PAYROOL

---
+++ main/java/com/emp/EmployeePayRollData.java
@@ -0,0 +1,19 @@
+package com.emp;
+
+public class EmployeePayRollData {
+    public int id;
+    public String name;
+    public double salary;
+
+    public EmployeePayRollData(int id, String name, double salary)
+    {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String toString()
+    {
+        return  "id = " + id + ", name=" + name + "\'"  +  ", salary" + salary;
+    }
+}
